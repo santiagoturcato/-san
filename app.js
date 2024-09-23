e641669f7dbb4f254d34b785729af67cf69c65b3
santiagoturcato <163873307+santiagoturcato@users.noreply.github.com>
2024-09-23 12:24:20 -0300

    .

---
+++ app.js
@@ -0,0 +1,17 @@
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao =document.createElement ('article')
+    cartao.innerHTML=`
+    <div class="cartao_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    let respostaEstaVisivel= false
+    function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+}
